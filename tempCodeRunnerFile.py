570177b1678bfe055c0ad7826a2a06a922ba3e04
liyemenOX <kushagratripathi491@gmail.com>
2026-05-25 15:52:18 +0530

    feat: added a partial search in the project

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -4,20 +4,26 @@ print("hey! there it is a emoji project")
 
 with open("emoji.json", "r", encoding = "utf-8") as file:
     emoji = json.load(file)
-
+    
+    requested = input("enter the emoji: ").strip().lower()
+            
 def print_requested_emoji():
     
-    requested = input("enter the emoji: ").lower()
+    for item in emoji: #looping through the entire list of emojis
+        if requested == item["name"] or requested == item["keywords"]:
+            
+            partial_search(item)
+            break      
     
-    if requested in emoji: #basically checking the dictionary
-        #now looping inside 
-        print(emoji[requested])
-                
     else:
-        print("emoji not found")#calling the function
+            print("emoji not found")#calling the function
+
+def partial_search(item):
+    
+    if requested in item["name"]:
+        print(item["emoji"])
 
 print_requested_emoji()
+        
 
-
-
-    
+            
