3b3578a229ff7a52b5d25e229c4c19b3b15d18eb
liyemenOX <kushagratripathi491@gmail.com>
2026-05-27 22:37:48 +0530

    improved the loops for partial_search and introduced a global variable

--- emoji_project.py
+++ emoji_project.py
@@ -6,24 +6,32 @@ with open("emoji.json", "r", encoding = "utf-8") as file:
     emoji = json.load(file)
     
     requested = input("enter the emoji: ").strip().lower()
+    
+    found_any_emoji = False
             
 def print_requested_emoji():
     
-    for item in emoji: #looping through the entire list of emojis
-        if requested in item["name"] in requested == item["keywords"]:
-            
-            partial_search(item)
-            break      
+    global found_any_emoji #introduced a global variable 
     
-    else:
-            print("emoji not found")#calling the function
-
+    for item in emoji:
+        
+        partial_search(item)
+            #looping through the entire list of emojis
+                
+    
+    if not found_any_emoji:
+        print("emoji not found")
+    
 def partial_search(item):
-    
-    if requested in item["name"]:
+
+    global found_any_emoji
+    if requested in item["name"] or requested in item["keywords"]: 
+        #starting to improve the search part of partial check 
         print(item["emoji"])
+    found_any_emoji = True
 
 print_requested_emoji()
-        
 
-            
+
+
+    
