6b42478d3e5828cab1ed278b47e99adb2235a92d
liyemenOX <kushagratripathi491@gmail.com>
2026-06-09 12:45:12 +0530

    feat: added classes for database and Emoji and removed the funct: print_requested_emoji

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,43 +1,67 @@
 import json
 
+class Emoji:
+    def __init__(self,name,emoji,keywords,category):
+        self.name = name
+        self.emoji = emoji
+        self.keywords = keywords
+        self.category = category
+        
+        
 print("hey! there it is a emoji project")
 
-with open("emoji.json", "r", encoding = "utf-8") as file:
-    emoji = json.load(file)
+class Emoji_Database:
+    def __init__(self,file_path):
+        self.file_path = file_path
+        
+        self.emoji = []
+        
+        self.load_emojis()
+        
+    def load_emojis(self):
+        with open("emoji.json", "r", encoding = "utf-8") as file:
+            emoji = json.load(file)
     
-    requested = input("enter the emoji: ").strip().lower()
-    
-    found_any_emoji = False
-            
-def print_requested_emoji():
-    
-    global found_any_emoji #introduced a global variable 
-    
-    multiple_result = partial_search()
-    
-    if not multiple_result:
-        print("no matching emoji found in database")
-        return     
-
-    for item in  multiple_result:
-        print(item["emoji"])
+        for item in emoji:
+            new_emoji_object = Emoji(item["name"],item["emoji"],item["keywords"],item["category"])
+            self.emoji.append(new_emoji_object)
     
     
-def partial_search():
-
-    multiple_result = []
-    
-    global found_any_emoji
-    for item in emoji:
-        for word in item["keywords"]:
-            if requested in item["name"]  or requested in word:
-        #starting to improve the search part of partial check 
-                found_any_emoji = True
+    def partial_search(self,requested):
+        
+        multiple_result = []
+        self.found_any_emoji = False
+         
+        for item in self.emoji:
+            if requested in item.name:
+                self.found_any_emoji = True
                 multiple_result.append(item)
-                break;
-    return multiple_result
-           
-print_requested_emoji()
+                continue
+            
+            for word in item.keywords:
+                if requested in word:#starting to improve the search part of partial check 
+                    self.found_any_emoji = True
+                    multiple_result.append(item)
+                    break;
+        return multiple_result
+    
+            
+db = Emoji_Database("emoji.json")
+
+requested = input("enter the emoji:").strip().lower()
+
+multiple_result = db.partial_search(requested)
+
+if db.found_any_emoji and multiple_result:
+    print("\nMatching Emojis:")
+    for emoji_obj in multiple_result:  
+        print(emoji_obj.emoji) 
+else:
+    print("\nNo matching emojs found!")
+
+
+
+
 
 
 
